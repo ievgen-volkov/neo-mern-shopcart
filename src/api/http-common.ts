e007de859c47969de1d11eb4090096352cc61f61
Ievgen Volkov <67655917+ievgen-volkov@users.noreply.github.com>
2023-02-02 18:46:59 +0200

    Update http-common.ts

--- src/api/http-common.ts
+++ src/api/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const request = axios.create({
-  baseURL: "https://neo-mern.herokuapp.com/api",
+  baseURL: "https://neo-mern-shoppingcart-server.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
